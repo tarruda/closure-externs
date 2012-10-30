7298a03d2f56bea98b665941c6174da810e86ca0
Thiago de Arruda <tpadilha84@gmail.com>
2012-10-30 17:29:14 -0300

    Added jquery terminal externs

---
+++ jquery-terminal.js
@@ -0,0 +1,21 @@
+/** @constructor */
+jQuery.terminal.options = function() {};
+
+jQuery.terminal.options.prototype.greetings;
+
+jQuery.terminal.options.prototype.tabcompletion;
+
+jQuery.terminal.options.prototype.name;
+
+jQuery.terminal.options.prototype.height;
+
+jQuery.terminal.options.prototype.prompt;
+
+jQuery.terminal.options.prototype.enabled;
+
+/**
+ * @param {=(object)} interpreter
+ * @param {=(string|jQuery.terminal.options)} termOpts
+ * @return {object}
+ */
+jQuery.prototype.terminal = function(interpreter, termOpts) {};
