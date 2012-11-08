fff58e80ac5690c1aa7c14bbe886fac08e39a092
Thiago de Arruda <tpadilha84@gmail.com>
2012-11-07 21:14:37 -0300

    Added handlebars externs

---
+++ handlebars-1.0.rc.1.js
@@ -0,0 +1,7 @@
+var Handlebars = {
+    compile: function(templateStr) {},
+    registerHelper: function(name, helperFn) {},
+    createFrame: function(data) {},
+    SafeString: function(str) {}
+};
+
